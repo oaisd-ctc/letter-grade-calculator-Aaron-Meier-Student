76b694c1b2b016acf9f8fef344534c6e2ec8d007
Aaron-Meier-Student <aamea72@student.oaisd.org>
2024-10-21 14:04:14 +0000

    Created the functionality to print the letter grade and send a motivational quote.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number score you have in class.");
+        int numberGrade = int.Parse(Console.ReadLine());
+        if (numberGrade >= 100) {
+            Console.WriteLine($"Letter Grade: A+");
+            Console.WriteLine("Wow! 100… That's impressive!");
+        } else if (numberGrade >= 90) {
+            Console.WriteLine($"Letter Grade: A");
+            Console.WriteLine("Very nice!");
+        } else if (numberGrade >= 80) {
+            Console.WriteLine($"Letter Grade: B");
+            Console.WriteLine("Keep it up!");
+        } else if (numberGrade >= 70) {
+            Console.WriteLine($"Letter Grade: C");
+            Console.WriteLine("You got this, keep working!");
+        } else if (numberGrade >= 60) {
+            Console.WriteLine($"Letter Grade: D");
+            Console.WriteLine("Keep trying!");
+        } else {
+            Console.WriteLine($"Letter Grade: E");
+            Console.WriteLine("Don't give up!");
+        }
+    }
+}
